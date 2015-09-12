cd290e292350999fcd04fe513518f810ff58c4ab
Ivan Mwanja <agentivan2000@gmail.com>
2015-09-11 18:03:55 -0700

    Update

--- plot_script.R
+++ plot_script.R
@@ -9,8 +9,12 @@ rm("raw_data")
 #Converting the Date field into a date format. 
 req_data[[1]] <- as.Date(req_data[[1]],"%d/%m/%Y")
 
-
-
 # test[[1]] <- as.Date(test[[1]],"%d/%m/%Y")
-
 # test[[2]] <- strptime(test[[2]],"%H:%M:%S")
+
+# Plotting the first graph
+png(filename = "plot1.png")
+hist(req_data$Global_active_power, col = "red", xlab = "Global Active Power (Kilowatts)",main = "Global Active Power")
+dev.off()
+
+# 
